1d4ba4f6ee5ce106921a5ce668ddd4a951032a22
ChhabriaHeena <heenachhabria38@gmail.com>
2024-09-20 00:42:18 +0530

    About COmponent

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,9 +1,14 @@
+import { Grid } from '@mui/material'
 import React from 'react'
 
 const About = () => {
   return (
     <div>
-      About
+      <h1>What I Do?</h1>
+      <Grid>
+        <Grid item></Grid>
+        <Grid item></Grid>
+      </Grid>
     </div>
   )
 }
