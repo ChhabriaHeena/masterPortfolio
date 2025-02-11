d7f1112f592b16253f9b6b5e0e424661523edc56
ChhabriaHeena <heenachhabria38@gmail.com>
2025-02-11 20:04:07 +0530

    lazy loading implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { Suspense, lazy } from "react";
 // import './App.css';
 import Portfolio from './components/Portfolio';
+
+const LazyPortfolio = lazy(()=> import('./components/Portfolio'))
 
 function App() {
   return (
     <div className="App">
-      <Portfolio/>
+      <Suspense fallback="Portfolio is Loading..."><LazyPortfolio/></Suspense>
+      {/* <Portfolio/> */}
     </div>
   );
 }
