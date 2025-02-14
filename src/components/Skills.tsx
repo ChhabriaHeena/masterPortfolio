2ac9f0f2666dba0b9bd378b3d9115755aa0ea525
ChhabriaHeena <heenachhabria38@gmail.com>
2025-02-14 21:42:29 +0530

    skills component modifications

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Skills = () => {
   return (
-    <div>
+    <div style={{textAlign:"center"}}>
       Skills (work experience)
       <h3>ReactJS TypeScript JavaScript HTML5 CSS3 SCSS BootStrap FrontEnd Development Azure Firebase MongoDB ExpressJS NodeJS NPM AWS</h3>
     </div>
