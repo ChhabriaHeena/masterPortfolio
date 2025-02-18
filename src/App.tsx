023c9632a5990afabc350854b06775b847c6aa81
ChhabriaHeena <heenachhabria38@gmail.com>
2025-02-19 00:04:39 +0530

    Loading Sign at the Beginning

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
-import { Suspense, lazy } from "react";
+import { Suspense, lazy, useEffect, useState } from "react";
 // import './App.css';
 import Portfolio from './components/Portfolio';
+import Loader from "./components/Loader";
 
-const LazyPortfolio = lazy(()=> import('./components/Portfolio'))
+const LazyPortfolio = lazy(() => import('./components/Portfolio'))
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000);
+
+  }, [loading])
   return (
     <div className="App">
-      <Suspense fallback="Portfolio is Loading..."><LazyPortfolio/></Suspense>
+      <Suspense fallback={<Loader loading={loading} />}><LazyPortfolio /></Suspense>
       {/* <Portfolio/> */}
     </div>
   );
