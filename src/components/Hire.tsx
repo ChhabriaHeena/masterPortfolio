a81eb4daae1b6f42ea2d32c242141b2016d2b886
ChhabriaHeena <heenachhabria38@gmail.com>
2025-02-23 19:39:29 +0530

    Hire Me Contact Button

--- src/components/Hire.tsx
+++ src/components/Hire.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useStyles from './styles/Hire.styles';
 import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 
 const Hire = () => {
@@ -13,7 +14,11 @@ const Hire = () => {
       <div className={classes.hireMain}>
         <h2>Hire Me</h2>
         <div>
-          <Button variant="contained">Contact</Button>
+          <Button variant="contained">
+            <Link style={{ color: '#fff', textDecoration: "none" }} to='/contact' target="_blank">
+              Contact
+            </Link>
+          </Button>
         </div>
       </div>
     </section>
