0c77a055faf5b5f2cde92e9e30abdad069ac2054
ChhabriaHeena <heenachhabria38@gmail.com>
2025-02-23 20:37:51 +0530

    About Content

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,12 +14,14 @@ const About = () => {
       <h1 className={classes.aboutHeader}>What I Do?</h1>
       <Grid container>
         <Grid item xs={12} md={5} sm
-        className={classes.aboutMeImage}>
+          className={classes.aboutMeImage}>
           <img className={classes.aboutImage} src={aboutMeImage} alt="image" />
         </Grid>
         <Grid item xs={12} md={6} sm={6} className={classes.aboutContent}>
           <h3>
-           A self-motivated individual with 2+ years of experience in Frontend Development and a great understanding of programming languages who is looking for a new opportunity. A position in a growth-oriented company where I can put my abilities to effective use. While having the benefit of the company this would be the opportunity to expand my knowledge and abilities
+            I've worked as a Software Engineer for more than 2+ years. I've demonstrated my abilities several times while working for a product-based start-up.
+
+            In addition to many other things, I have worked on React JS, MUI, interface design, code debugging, front-end development, CSS, SCSS, JavaScript, TypeScript, JSON, HTML, and many more. I have also created React libraries and integrated APIs.
           </h3>
 
         </Grid>
